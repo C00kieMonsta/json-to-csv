3a4199339d5845dd3055a324f5c2954e3ec2b636
Antoine <karagan.app@gmail.com>
2019-05-29 16:59:24 +0200

    adding functionality to convert xsls to json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-let converter = require('json-2-csv');
+const json2csv = require('json-2-csv');
+const xlsx = require('xlsx');
 const fs = require('fs');
 
-const jsonFile = require('../assets/test.json');
 
-let json2csvCallback = function (err, csv) {
-    if (err) throw err;
-    fs.writeFile('csv/temp.csv', csv, function(err, data){
-        if (err) console.log(err);
-        console.log("Successfully Written to File.");
+//
+// JSON 2 CSV
+//
+
+
+function jsonToCsv(file) {
+    json2csv.json2csv(file, function (err, csv) {
+        if (err) throw err;
+        fs.writeFile('csv/temp.csv', csv, function (err, data) {
+            if (err) console.log(err);
+            console.log("Successfully Written to File.");
+        });
     });
-};
+}
 
-converter.json2csv(jsonFile, json2csvCallback);
+// const jsonFile = require('../assets/test.json');
+// jsonToCsv(jsonFile):
+
+
+//
+// EXCEL TO JSON
+//
+function xlsxToJSON(path) {
+    const buffers = [];
+    fs.createReadStream(path).on('error', (error) => {
+        console.log(error);
+    }).on('data', (chunk) => {
+        buffers.push(chunk);
+    }).on('end', () => {
+        const buffer = Buffer.concat(buffers);
+        const workbook = xlsx.read(buffer, { type: 'buffer', cellDates: true });
+        const worksheet = xlsx.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { raw: true });    
+        fs.writeFile('json/temp.json',  JSON.stringify(worksheet), function (err, data) {
+            if (err) console.log(err);
+            console.log("Successfully Written to File.");
+        });
+    });
+}
+xlsxToJSON('./assets/test.xlsx');
